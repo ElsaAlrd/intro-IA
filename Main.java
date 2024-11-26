1b6133f177521ace3e1455cc468b203d5f71c574
ElsaAlrd <119571053+ElsaAlrd@users.noreply.github.com>
2024-11-26 18:30:15 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,66 +1,28 @@
-package tp1_vEtudiants.exercice1;
+package algorithmes;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-import tp1_vEtudiants.exercice1.Epreuve;
-import tp1_vEtudiants.exercice1.ListeEpreuves;
+import problemes.ChaineBinaire;
+import problemes.Probleme;
+import representation.Solution;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		String fichierEpreuves = "C:\\Users\\elsa-\\OneDrive\\Documents\\esirem\\4A\\S1\\intro IA\\tp1\\ex1\\Epreuves.txt";
+		int taille_population = 50; 
+		int nb_generations = 2000; 
 		
-		ListeEpreuves listeEpreuves = new ListeEpreuves(fichierEpreuves);
-
-		System.out.println("#################################################");
-		System.out.println("# Toutes les epreuves inscrites dans le fichier #");
-		System.out.println("#################################################");
+		double probaCroisement = .8; 
+		double probaMutation = 0.1;
 		
-		System.out.println(listeEpreuves);
-		System.out.println();
+		int nbBits = 16;
+		Probleme probleme; 
+		probleme = new ChaineBinaire(nbBits); 
 		
-		listeEpreuves.triParHeureFin();
+		Algorithme_genetique algo = new Algorithme_genetique(probleme, taille_population, nb_generations, probaCroisement, probaMutation); 
+		Solution best = algo.executer();
 		
-		System.out.println("#################################################");
-		System.out.println("# Epreuves triees par horaires de fin croissant #");
-		System.out.println("#################################################");
+		System.out.println("Resultat: " + best);
 		
-		System.out.println(listeEpreuves);
-		System.out.println();
-		
-		Epreuve e = listeEpreuves.get(4); //Choix de l'epreuve a eliminer les conflits selon l'odre dans la liste triée par ordre de fin croissant
-		listeEpreuves.eliminerConflits(e); //Elimination des conflits de l'epreuve e
-		
-		//Affichage des epreuves apres suppression des conflits de l'epreuve e
-		System.out.println("#################################################");
-		System.out.println("#  Epreuves apres suppression des conflits de   #");
-		System.out.println("#              l'epreuve de " +  e.getIntitule() + "             #");
-		System.out.println("#################################################");
-		
-		//Affichage de toutes les epreuves
-		System.out.println(listeEpreuves);
-		System.out.println();
-		
-				
-		ArrayList<Epreuve> planning = new ArrayList<Epreuve>();
-		for (int j = 0; j < listeEpreuves.getListe().size(); j++){
-		planning.add(listeEpreuves.get(j));
-		listeEpreuves.eliminerConflits(listeEpreuves.get(j));}
-		
-		
-		Collections.sort((List<Epreuve>) planning);
-		
-		System.out.println("#################################################");
-		System.out.println("#             Planning des epreuves             #");
-		System.out.println("#################################################");
-		
-		for (int i = 0; i < planning.size(); i++) {
-			System.out.println(planning.get(i));
-		}
 	}
-
 
 }
